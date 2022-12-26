2360fa937c9380f21fd979f3b367ed80be1deb03
leirdan <noahtpp2@gmail.com>
2022-12-26 15:01:29 -0300

    Funções de renderizar telas de produtos e vendedores

--- app/Controllers/Http/SalesmenController.ts
+++ app/Controllers/Http/SalesmenController.ts
@@ -1,9 +1,21 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Salesman from "App/Models/Salesman";
 
 export default class SalesmenController {
-  public async index({}: HttpContextContract) {}
+  public async index({ view, request }: HttpContextContract) {
+    const id = request.param("id");
+    await Salesman.find(id)
+      .then((user) => {
+        return view.render("salesman/index", { salesman: user });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
-  public async create({}: HttpContextContract) {}
+  public async create({ view }: HttpContextContract) {
+    return view.render("salesman/create");
+  }
 
   public async store({}: HttpContextContract) {}
 
